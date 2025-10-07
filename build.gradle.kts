7345e4c75d3f0b70cf6d7688c4d0a15e8ea93fd8
Takanori Ugai <ugai@fujitsu.com>
2025-10-07 14:28:43 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,104 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
+plugins {
+    kotlin("jvm") version "2.2.20"
+    id("com.gradleup.shadow") version "9.1.0"
+    id("org.jetbrains.dokka") version "2.0.0"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
+    id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
+    id("com.github.jk1.dependency-license-report") version "2.9"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("dev.langchain4j:langchain4j:1.7.1")
+    implementation("dev.langchain4j:langchain4j-open-ai:1.7.1")
+    testImplementation(kotlin("test"))
+}
+
+tasks {
+    compileKotlin {
+        compilerOptions.jvmTarget = JvmTarget.JVM_21
+    }
+
+    compileTestKotlin {
+        compilerOptions.jvmTarget = JvmTarget.JVM_21
+    }
+    test {
+        useJUnitPlatform()
+    }
+
+    withType<Detekt>().configureEach {
+        // Target version of the generated JVM bytecode. It is used for type resolution.
+        jvmTarget = "21"
+        reports {
+            // observe findings in your browser with structure and code snippets
+            html.required.set(true)
+            // checkstyle like format mainly for integrations like Jenkins
+            xml.required.set(true)
+            // similar to the console output, contains issue signature to manually edit baseline files
+            txt.required.set(true)
+            // standardized SARIF format (https://sarifweb.azurewebsites.net/) to support integrations
+            // with Github Code Scanning
+            sarif.required.set(true)
+        }
+    }
+
+    val execute by registering(JavaExec::class) {
+        group = "application"
+        mainClass.set(
+            if (project.hasProperty("mainClass")) {
+                project.property("mainClass") as String
+            } else {
+                application.mainClass.get()
+            },
+        )
+        classpath = sourceSets.main.get().runtimeClasspath
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+application {
+    mainClass.set("SaladSampleKt")
+    applicationDefaultJvmArgs = listOf("--add-modules=jdk.incubator.vector")
+}
+
+ktlint {
+    verbose.set(true)
+    outputToConsole.set(true)
+    coloredOutput.set(true)
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
+        reporter(ReporterType.JSON)
+        reporter(ReporterType.HTML)
+    }
+    filter {
+        exclude("**/style-violations.kt")
+    }
+}
+
+detekt {
+    buildUponDefaultConfig = true // preconfigure defaults
+    allRules = false // activate all available (even unstable) rules.
+    config.setFrom("$projectDir/config/detekt.yml")
+}
+
+dokka.dokkaSourceSets {
+    configureEach {
+        jdkVersion.set(21)
+        enableJdkDocumentationLink.set(false)
+        enableKotlinStdLibDocumentationLink.set(false)
+    }
+}
