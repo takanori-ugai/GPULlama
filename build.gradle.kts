c87ea6b909935bfacb015d926f564bf06962bfad
Takanori Ugai <ugai.takanori@gmail.com>
2026-05-19 10:19:15 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ repositories {
 
 dependencies {
     implementation("dev.langchain4j:langchain4j:1.15.0")
-    implementation("dev.langchain4j:langchain4j-gpu-llama3:1.15.0-beta24")
+    implementation("dev.langchain4j:langchain4j-gpu-llama3:1.15.0-beta25")
     implementation("dev.langchain4j:langchain4j-open-ai:1.15.0")
     implementation("tornado:tornado-matrices:1.1.1")
     implementation("tornado:tornado-api:1.1.1")
